9dff596462d58770a6dc1ffb14d2dbd1c9b71215
zengyuxing007 <newday.jesse@gmail.com>
2010-08-20 02:52:00 -0700

    my first ci using web github

--- hellworld.cpp
+++ hellworld.cpp
@@ -5,5 +5,7 @@ int main()
 {
 	cout<<"helloworld"<<endl;
 	cout<<"my name is zengyuxing"<<endl;
+        cout<<"fuck github"<<endl;
+        cout<<"github is a good soft source version manager"<<endl;
 	return 0;
 }
