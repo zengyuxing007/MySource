4d68ac8df333b3d15416bfac34e8bd6cf7a368bf
jesse <newday.jesse@gmail.com>
2010-10-28 00:18:09 +0800

    using github on my ubuntu os

--- hellworld.cpp
+++ hellworld.cpp
@@ -7,5 +7,6 @@ int main()
 	cout<<"my name is zengyuxing"<<endl;
         cout<<"fuck github"<<endl;
         cout<<"github is a good soft source version manager"<<endl;
+	cout<<"now,i am using github on my ubuntu os"<<endl;
 	return 0;
 }
